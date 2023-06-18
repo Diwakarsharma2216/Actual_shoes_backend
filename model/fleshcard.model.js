546d86f1d353c5b28d79c6994cb8320571b33799
diwakar01 <diwakarsharma2216@gmail.com>
2023-06-18 22:10:45 +0530

    update-final-update

--- model/fleshcard.model.js
+++ model/fleshcard.model.js
@@ -3,7 +3,6 @@ const { Schema, model } = require("mongoose");
 const felshSchema=Schema({
     title:String,
     description:String,
-    diagram:String,
     term:String,
     definition:String,
     userID:String,
